6c61013e2afcd75c1d45b713c9d7297887bc7721
Alberto Lopez <albertolopez19@gmail.com>
2016-09-02 12:15:38 -0400

    Update main nav component, render child components

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
-const App = () => {
-    return <div>Hello World</div>
+class App extends React.Component {
+    render() {
+        console.log(this);
+        return (
+            <div className="main-container">
+                <nav className="navbar navbar-default" role="navigation">
+                    <div className="col-sm-7 col-sm-offset-2" style={{marginTop: 15}}>
+                        MENU
+                    </div>
+                </nav>
+                <div className="container">
+                    {this.props.children}
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
--- app/config/routes.js
+++ app/config/routes.js
@@ -3,8 +3,10 @@ import App from '../components/App.jsx';
 import Home from '../components/Home.jsx';
 import { Router, IndexRoute, hashHistory, Route } from 'react-router';
 
-module.exports = (
+const routes = (
     <Route path="/" component={App}>
         <IndexRoute component={Home} />
     </Route>
 );
+
+export default routes;
