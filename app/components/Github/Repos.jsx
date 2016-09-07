eeacb7dc3116b2aca279eb886a318a1be276fd4c
Alberto Lopez <albertolopez19@gmail.com>
2016-09-07 11:40:05 -0400

    Display user repo and profile information

--- app/components/Github/Repos.jsx
+++ app/components/Github/Repos.jsx
@@ -3,10 +3,20 @@ import React from 'react';
 
 export default class Repos extends React.Component {
     render() {
-        console.log(this.props.repos);
+        const repos = this.props.repos.map((repo, index) => {
+            return (
+                <li className="list-group-item" key={index}>
+                    {repo.html_url && <h4><a href={repo.html_url}>{repo.name}</a></h4>}
+                    {repo.description && <p>{repo.description}</p>}
+                </li>
+            )
+        });
         return (
             <div>
-                <p>Repos</p>
+                <h3>User Repositories</h3>
+                <ul className="list-group">
+                    {repos}
+                </ul>
             </div>
         )
     }
