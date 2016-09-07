5ba82bfaadc00603b235faa3262947dab0e5e719
Alberto Lopez <albertolopez19@gmail.com>
2016-09-07 13:47:00 -0400

    Add docs branch and update production webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,13 @@ const path = require('path');
 const PATHS = {
     public: path.join(__dirname, 'public')
 }
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const pkg = require('./package.json');
+const TARGET = process.env.npm_lifecycle_event;
+process.env.BABEL_ENV = TARGET;
 
-module.exports = {
+const common = {
     entry: {
         javascript: './app/components/main.js',
     },
@@ -11,12 +16,6 @@ module.exports = {
         path: PATHS.public,
         filename: 'bundle.js',
         publicPath: '/public/',
-    },
-    devServer: {
-        contentBase: PATHS.public,
-        inline: true,
-        port: 3333,
-        historyApiFallback: true
     },
     module: {
         loaders: [
@@ -29,5 +28,40 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+};
+
+// Default config
+if (TARGET === 'start' || !TARGET) {
+	module.exports = merge(common, {
+		devtool: 'eval-source-map',
+        devServer: {
+            contentBase: PATHS.public,
+            inline: true,
+            port: 3333,
+            historyApiFallback: true
+        },
+		plugins: [
+			new webpack.HotModuleReplacementPlugin(),
+		]
+	});
+}
+
+if (TARGET === 'build' || TARGET === 'stats') {
+	module.exports = merge(common, {
+        devtool: 'cheap-module-source-map',
+		plugins: [
+			new webpack.DefinePlugin({
+				'process.env.NODE_ENV': '"production"'
+			}),
+			new webpack.optimize.UglifyJsPlugin({
+				compress: {
+					warnings: false,
+                    screw_ie8: true
+				},
+                comments: false,
+                sourceMap: false
+			}),
+		]
+	});
 }
